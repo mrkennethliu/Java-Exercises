5c7dbf48d443e01b3f908b2492842adadad79545
mrkennethliu <mrkennethliu@users.noreply.github.com>
2014-07-05 07:38:00 -0400

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Monster {
+    
+    public int numLegs;
+    public int numArms;
+    public String name;
+    
+    public Monster (int legs, int arms, String title){
+        numLegs = legs;
+        numArms = arms;
+        name = title;
+    }
+}
+    
